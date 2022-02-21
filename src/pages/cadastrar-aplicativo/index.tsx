59383bf2cb5c9e995c457912e7eca2a81316f5dd
TauDuque <hectauduque@gmail.com>
2022-02-21 20:40:20 -0300

    subj : new page cadastrar-aplicativo

---
+++ src/pages/cadastrar-aplicativo/index.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import { useSession } from 'next-auth/client';
+import { useToasts } from '@geist-ui/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import BotaoVoltar from '@components/BotaoVoltar';
+import { AddBtn } from '@styles/buttons';
+import { InputStyles, RowStyle, Form, InputBoxStyle } from './style';
+
+export default function CadastrarAplicativo() {
+  const [, setToast] = useToasts();
+  const [session] = useSession();
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>Orion | Cadastro de Aplicativos</title>
+      </Head>
+      <BotaoVoltar />
+      <h2>Cadastro de Aplicativos</h2>
+      <AddBtn>
+        <button>Confirmar</button>
+      </AddBtn>
+      <Form>
+        <RowStyle>
+          <InputStyles>
+            <label htmlFor="nome">Nome do Aplicativo</label>
+            <input type="text" id="nome" />
+          </InputStyles>
+          <InputStyles>
+            <label htmlFor="categoria">Categoria</label>
+            <input type="text" id="categoria" />
+          </InputStyles>
+        </RowStyle>
+        <InputStyles>
+          <label htmlFor="descricao">Descrição</label>
+          <input type="text" id="descricao" className="description" />
+        </InputStyles>
+        <InputBoxStyle>
+          <div>
+            <InputStyles>
+              <label htmlFor="descricao">Título Chave 1</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 2</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 3</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 4</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 5</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 6</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 7</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Chave 8</label>
+              <input type="text" id="descricao" className="chave" />
+            </InputStyles>
+          </div>
+          <div>
+            <InputStyles>
+              <label htmlFor="descricao">Título Dado Alfa 1</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 2</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 3</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 4</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 5</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 6</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 7</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Alfa 8</label>
+              <input type="text" id="descricao" className="chave" />
+            </InputStyles>
+          </div>
+          <div>
+             <InputStyles>
+              <label htmlFor="descricao">Título Dado Num. 1</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Num. 2</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Num. 3</label>
+              <input type="text" id="descricao" className="chave" />
+              <label htmlFor="descricao">Título Dado Num. 4</label>
+              <input type="text" id="descricao" className="chave" />
+            </InputStyles>
+          </div>
+        </InputBoxStyle>
+      </Form>
+    </>
+  );
+}
+
+CadastrarAplicativo.auth = true;
---
+++ src/pages/cadastrar-aplicativo/style.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const Form = styled.div`
+  padding: 10px 15px;
+`;
+
+export const RowStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const InputStyles = styled.div`
+  margin-top: 10px;
+  label {
+    padding: 0 0.5rem;
+    font-weight: bold;
+  }
+  input,
+  label {
+    display: block;
+  }
+
+  input {
+    min-width: 440px;
+    width: 100%;
+    height: 32px;
+    border: 1px solid #eaeaea;
+    letter-spacing: 0.04rem;
+    padding-inline-start: 8px;
+  }
+  .description {
+    min-width: 220px;
+    width: 100%;
+    border: 1px solid #eaeaea;
+    height: 32px;
+    padding-inline-start: 8px;
+  }
+
+  .chave {
+    min-width: 100px;
+    width: 300px;
+    /* width: 100%; */
+    border: 1px solid #eaeaea;
+    height: 32px;
+    padding-inline-start: 8px;
+  }
+
+  @media (max-width: 1185px) {
+    input {
+      max-width: 280px;
+      min-width: 150px;
+    }
+  }
+
+  /* @media (max-width: 1015px) {
+    input {
+      width: 65%;
+      min-width: 280px;
+    }
+  }
+
+  @media (max-width: 870px) {
+    input {
+      width: 45%;
+      min-width: 200px;
+    }
+  } */
+`;
+
+export const InputBoxStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 12px;
+  margin-top: 25px;
+
+  > div {
+    border: 1px solid #eaeaea;
+    width: 365px;
+    padding: 8px;
+  }
+`;
