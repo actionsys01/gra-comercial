53aa174b273e27bdbc1df5e53a86479c7d5b8f5c
TauDuque <hectauduque@gmail.com>
2022-02-21 19:25:04 -0300

    subj : new query to request cadastro-categorias get by code

--- src/services/categorias/cadastro-categorias/GetCategoryByService.ts
+++ src/services/categorias/cadastro-categorias/GetCategoryByService.ts
@@ -1,9 +1,13 @@
 import api from '@services/api';
 
-export default async function GetCategoryByService(cod_categoria: string) {
+export default async function GetCategoryByService(
+  cod_categoria: string,
+  page: number,
+) {
   const response = await api.get('/categorias', {
     params: {
       cod_categoria,
+      page,
     },
   });
 
