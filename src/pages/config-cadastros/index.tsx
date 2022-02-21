9d998203f50a2b89f94d84978a92653f403706e3
TauDuque <hectauduque@gmail.com>
2022-02-21 16:47:42 -0300

    subj : new page config-cadastros

--- src/pages/config-cadastros/index.tsx
+++ src/pages/config-cadastros/index.tsx
@@ -3,16 +3,148 @@ import { useSession } from 'next-auth/client';
 import { useToasts } from '@geist-ui/react';
 import Head from 'next/head';
 import { Plus, Filter } from '@geist-ui/react-icons';
+import { Pages } from '@styles/pages';
+import Pagination from '@material-ui/lab/Pagination';
 import { useRouter } from 'next/router';
 import { TableGrid } from '@styles/tableStyle';
 import Popover from '@components/Popover';
 import Loader from '@components/Loader';
 import { BtnRow } from '@styles/buttons';
+import * as request from '@services/categorias';
+import { IConfigData } from '@services/categorias/cadastro-config/types';
+import BotaoVoltar from '@components/BotaoVoltar';
 
-export default function index() {
+export interface IData {
+  aplicacao: string;
+  desc_aplicacao: string;
+  option: JSX.Element;
+}
+
+export default function ConfigCadastros() {
   const [, setToast] = useToasts();
   const [session] = useSession();
   const router = useRouter();
 
-  return <></>;
+  const [data, setData] = useState<IConfigData[]>([]);
+  const [page, setPage] = useState(1);
+  const [quantityPage, setQuantityPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [visibleModal, setVisibleModal] = useState(false);
+  const cod = '';
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  console.log('router.query', router.query);
+
+  const getConfigByCategoryCodePageData = useCallback(async () => {
+    try {
+      const response = await request.GetCategoryByService(
+        String(router.query.cod),
+      );
+      const data = response.data;
+      console.log('data', data.cadastro_config_id);
+      setData(data.cadastro_config_id);
+    } catch (error) {
+      console.log(error);
+      setToast({
+        text: 'Houve um problema. Por favor tente novamente',
+        type: 'warning',
+      });
+    }
+  }, []);
+
+  const gatheredData = useMemo(() => {
+    const allData: IData[] = [];
+    if (data) {
+      data.forEach((item, i) => {
+        allData.push({
+          ...item,
+          option: (
+            <Popover
+              num={i}
+              content={[
+                {
+                  optionName: 'Selecionar',
+                  onClick: () => console.log('Selecionar'),
+                  className: 'able',
+                },
+                {
+                  optionName: 'Editar',
+                  onClick: () => console.log('Editar'),
+                  className: 'able',
+                },
+                {
+                  optionName: 'Excluir',
+                  onClick: () => console.log('Excluir'),
+                  className: 'able',
+                },
+              ]}
+            />
+          ),
+        });
+      });
+    }
+    return allData;
+  }, [data]);
+
+  useEffect(() => {
+    getConfigByCategoryCodePageData();
+    console.log('data', data);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Orion | Aplicativos</title>
+      </Head>
+      <BotaoVoltar />
+      <h2>Aplicativos</h2>
+      <BtnRow>
+        <button>
+          <span>
+            <Filter />
+          </span>
+          Filtrar
+        </button>
+        <button>
+          <span>
+            <Plus />
+          </span>
+          Adicionar
+        </button>
+      </BtnRow>
+      <TableGrid>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Nome</th>
+              <th>Descrição</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gatheredData.map((item, i) => (
+              <tr key={i}>
+                <td>{item.option}</td>
+                <td>{item.aplicacao}</td>
+                <td>{item.desc_aplicacao}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </TableGrid>
+
+      <Pages>
+        <Pagination
+          onChange={handleChange}
+          count={quantityPage}
+          shape="rounded"
+        />
+      </Pages>
+    </>
+  );
 }
+
+ConfigCadastros.auth = true;
