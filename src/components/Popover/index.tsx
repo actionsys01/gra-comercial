b33b13fcc7d02ff5a8d1282e3b74043bd510b58d
TauDuque <hectauduque@gmail.com>
2022-02-21 09:33:58 -0300

    subj: popover adjustments

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -1,27 +1,90 @@
-import React, { useState } from 'react'
-import { PopStyle } from './style'
-import { BsThreeDots } from "react-icons/bs";
+import React, { useState } from 'react';
+import { PopStyle, PopStyleDouble, PopStyleFourLines } from './style';
+import { BsThreeDots } from 'react-icons/bs';
 
 const Popover = (props: any) => {
-    
-    const [visible, setVisible] = useState(false);
+  // console.log(`props`, props.num )
+  // console.log(`props.quant`, props.quant)
+  const [visible, setVisible] = useState(false);
 
-    const visibleHandler = () => {
-        setVisible(!visible)
-    }
+  const visibleHandler = () => {
+    setVisible(!visible);
+  };
 
-    return <>
-            <PopStyle>
-                <BsThreeDots onClick={visibleHandler} /> 
-                {visible && 
-                <div onMouseLeave={() => setVisible(false)}>
-                    {props?.content?.map((item: any, i: any) => (
-                        <p key={i} onClick={item.onClick}>{item.optionName}</p>
-                    ))}
-                    
-                </div>}
-            </PopStyle>
-        </>
-}
+  if (props.quant === 2) {
+    return (
+      <>
+        <PopStyleDouble>
+          <BsThreeDots onClick={visibleHandler} />
+          {visible && (
+            <div
+              onMouseLeave={() => setVisible(false)}
+              /*  className={props.num === 7 ? "last-prop" : props.num === 6 ? "beforeLast-prop" : ""} */
+            >
+              {props?.content?.map((item: any, i: any) => (
+                <p key={i} onClick={item.onClick}>
+                  {item.optionName}
+                </p>
+              ))}
+            </div>
+          )}
+        </PopStyleDouble>
+      </>
+    );
+  }
 
-export default Popover
+  if (props.quant === 4) {
+    return (
+      <>
+        <PopStyleFourLines>
+          <BsThreeDots onClick={visibleHandler} />
+          {visible && (
+            <div
+              onMouseLeave={() => setVisible(false)}
+              className={
+                props.num === 7
+                  ? 'last-prop'
+                  : props.num === 6
+                  ? 'beforeLast-prop'
+                  : ''
+              }
+            >
+              {props?.content?.map((item: any, i: any) => (
+                <p className={item.className} key={i} onClick={item.onClick}>
+                  {item.optionName}
+                </p>
+              ))}
+            </div>
+          )}
+        </PopStyleFourLines>
+      </>
+    );
+  }
+  return (
+    <>
+      <PopStyle>
+        <BsThreeDots onClick={visibleHandler} />
+        {visible && (
+          <div
+            onMouseLeave={() => setVisible(false)}
+            className={
+              props.num === 7
+                ? 'last-prop'
+                : props.num === 6
+                ? 'beforeLast-prop'
+                : ''
+            }
+          >
+            {props?.content?.map((item: any, i: any) => (
+              <p className={item.className} key={i} onClick={item.onClick}>
+                {item.optionName}
+              </p>
+            ))}
+          </div>
+        )}
+      </PopStyle>
+    </>
+  );
+};
+
+export default Popover;
--- src/components/Popover/style.tsx
+++ src/components/Popover/style.tsx
@@ -1,15 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const PopStyle = styled.div`
-    position: relative;
-    cursor: pointer;
-    div /* primeira div */ {
+  position: relative;
+  cursor: pointer;
+
+  div /* primeira div */ {
     position: absolute;
     width: -webkit-fill-available;
-    min-width: 85px;
+    min-width: min-content;
     height: fit-content;
-    bottom: -1.865rem;
-    left: 26.89825px;
+    min-height: 90px;
+    bottom: -2.865rem;
+    left: 26.99825px;
     background-color: #fff;
     color: #1c496a;
     border-radius: 5px;
@@ -17,44 +19,187 @@ export const PopStyle = styled.div`
     box-shadow: 0 8px 30px rgb(0 0 0 / 12%);
     border: none;
     box-sizing: border-box;
-    padding: 8pt 0;
+    padding: 8pt 12pt;
     text-align: center;
-    
-        p {
-            cursor: pointer;
-            margin: 0;
-            font-size: 0.875rem;
-            line-height: 2rem;
-        }
-}
 
-div:before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 42%;
-        width: 14px;
-        height: 2px;
-        border: 11px solid transparent;
-        border-right-color: #fff;
-        border-left: 0;
-        margin-top: -6px;
-        margin-left: -11px;
+    .able {
+      cursor: pointer;
+      margin: 0;
+      font-size: 0.875rem;
+      line-height: 2rem;
+    }
+    .disabled {
+      opacity: 0;
+      margin: 0;
+      font-size: 0.875rem;
+      line-height: 2rem;
+      color: #eaeaea;
+      cursor: default;
+    }
+  }
+
+  div:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 42%;
+    width: 14px;
+    height: 2px;
+    border: 11px solid transparent;
+    border-right-color: #fff;
+    border-left: 0;
+    margin-top: -4px;
+    margin-left: -11px;
+  }
+
+  .beforeLast-prop {
+    bottom: -1.565rem;
+  }
+
+  .beforeLast-prop:before {
+    margin-top: 17px;
+  }
+
+  .last-prop {
+    bottom: -0.565rem;
+  }
+
+  .last-prop:before {
+    margin-top: 33px;
+  }
+`;
+
+export const PopStyleDouble = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  div /* primeira div */ {
+    position: absolute;
+    width: -webkit-fill-available;
+    min-width: min-content;
+    height: fit-content;
+    min-height: 90px;
+    bottom: -1.2rem;
+    left: 26.99825px;
+    background-color: #fff;
+    color: #1c496a;
+    border-radius: 5px;
+    z-index: 1000;
+    box-shadow: 0 8px 30px rgb(0 0 0 / 12%);
+    border: none;
+    box-sizing: border-box;
+    padding: 8pt 12pt;
+    text-align: center;
+
+    p {
+      cursor: pointer;
+      margin: 0;
+      font-size: 0.875rem;
+      line-height: 2rem;
+    }
+  }
+
+  div:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 42%;
+    width: 14px;
+    height: 2px;
+    border: 11px solid transparent;
+    border-right-color: #fff;
+    border-left: 0;
+    margin-top: 7px;
+    margin-left: -11px;
+  }
+
+  .beforeLast-prop {
+    bottom: -1.565rem;
+  }
+
+  .beforeLast-prop:before {
+    margin-top: 17px;
+  }
+
+  .last-prop {
+    bottom: -0.565rem;
+  }
+
+  .last-prop:before {
+    margin-top: 33px;
+  }
+`;
+
+export const PopStyleFourLines = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  div /* primeira div */ {
+    position: absolute;
+    width: -webkit-fill-available;
+    min-width: min-content;
+    height: fit-content;
+    min-height: 90px;
+    bottom: -5.2rem;
+    left: 26.99825px;
+    background-color: #fff;
+    color: #1c496a;
+    border-radius: 5px;
+    z-index: 1000;
+    box-shadow: 0 8px 30px rgb(0 0 0 / 12%);
+    border: none;
+    box-sizing: border-box;
+    padding: 8pt 12pt;
+    text-align: center;
+
+    .able {
+      cursor: pointer;
+      margin: 0;
+      font-size: 0.875rem;
+      line-height: 2rem;
+    }
+    .disabled {
+      opacity: 0;
+      margin: 0;
+      font-size: 0.875rem;
+      line-height: 2rem;
+      color: #eaeaea;
+      cursor: default;
     }
 
-    /* .beforeLast-prop {
-            bottom: -1.565rem;
+    a {
+      text-decoration: none;
+      color: inherit;
     }
+  }
 
-    .beforeLast-prop:before {
-        margin-top: 17px;
-    }
+  div:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 42%;
+    width: 14px;
+    height: 2px;
+    border: 11px solid transparent;
+    border-right-color: #fff;
+    border-left: 0;
+    margin-top: -22px;
+    margin-left: -11px;
+  }
 
-    .last-prop {
-            bottom: -.565rem;
-    }
+  .beforeLast-prop {
+    bottom: -1.565rem;
+  }
 
-    .last-prop:before {
-        margin-top: 33px;
-    } */
-`
+  .beforeLast-prop:before {
+    margin-top: 36px;
+  }
+
+  .last-prop {
+    bottom: -0.565rem;
+  }
+
+  .last-prop:before {
+    margin-top: 52px;
+  }
+`;
