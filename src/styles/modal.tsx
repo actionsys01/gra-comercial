be08aa0395f6237810109d20d5eef42a4661206f
TauDuque <hectauduque@gmail.com>
2022-02-21 12:17:03 -0300

    subj : modal-styleseslint fix

--- src/styles/modal.tsx
+++ src/styles/modal.tsx
@@ -1,76 +1,74 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Modal = styled.div`
-z-index: 1000;
-display: block;
-position: fixed;
-top: 0;
-left: 0;
-height: 100vh;
-width:100vw;
-background: rgba(0,0,0,0.20);
-
->  div {
-width: 33%;
-  height: 25%;
-border: none;
-position: absolute;
-left: 40%;
-top: 30%;
-background-color: #fff;
-
-
-h4 {
-  font-size: 1.25rem; 
-  font-weight: 400;
-  margin: 20px auto;
-  padding: 0 1rem;
-  vertical-align: middle;
-  text-align: center ;
-  display: inline-block;
-}
-
-h6 {
-  font-weight: 400;
-  margin: 20px auto;
-  vertical-align: middle;
-  text-align: center ;
-  display: inline-block;
-  padding: 0 1rem;
-}
-
-div {
-  display: flex;
-  overflow: hidden;
-  width: 100%;
-  height: 3.625rem;
-  position: absolute;
-  bottom: 0;
+  z-index: 1000;
+  display: block;
+  position: fixed;
+  top: 0;
   left: 0;
-  right: 0;
-  border-top: 1px solid #eaeaea;
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.2);
 
->  button {
-      font-size: 0.75rem;
-      border: none;
-      color: #1C496A;
+  > div {
+    width: 33%;
+    height: 25%;
+    border: none;
+    position: absolute;
+    left: 40%;
+    top: 30%;
+    background-color: #fff;
+
+    h4 {
+      font-size: 1.25rem;
+      font-weight: 400;
+      margin: 20px auto;
+      padding: 0 1rem;
+      vertical-align: middle;
+      text-align: center;
+      display: inline-block;
+    }
+
+    h6 {
+      font-weight: 400;
+      margin: 20px auto;
+      vertical-align: middle;
+      text-align: center;
+      display: inline-block;
+      padding: 0 1rem;
+    }
+
+    div {
       display: flex;
-      align-items: center;
-      justify-content: center;
-      flex: 1;
-      height: 100%;
-      border-radius: 0;
-      min-width: 0;
-      background-color: #fff;
-      cursor: pointer;
-}
+      overflow: hidden;
+      width: 100%;
+      height: 3.625rem;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      border-top: 1px solid #eaeaea;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
 
-button:last-child{
-  border-left: 1px solid #eaeaea;
-}
+      > button {
+        font-size: 0.75rem;
+        border: none;
+        color: #1c496a;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+        height: 100%;
+        border-radius: 0;
+        min-width: 0;
+        background-color: #fff;
+        cursor: pointer;
+      }
 
-}
-}
-`
+      button:last-child {
+        border-left: 1px solid #eaeaea;
+      }
+    }
+  }
+`;
