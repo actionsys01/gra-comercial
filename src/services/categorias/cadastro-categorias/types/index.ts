fa6e44b72f2d78c9d41683b16a74b7328d9c0e4e
TauDuque <hectauduque@gmail.com>
2022-02-21 16:48:46 -0300

    subj : cadastro-categoria types services

--- src/services/categorias/cadastro-categorias/types/index.ts
+++ src/services/categorias/cadastro-categorias/types/index.ts
@@ -1,4 +1,12 @@
+import { IConfigData } from '@services/categorias/cadastro-config/types';
+
 export interface ICategories {
   cod_categoria: string;
   desc_categoria: string;
+}
+
+export interface ICategoriesApps {
+  cod_categoria: string;
+  desc_categoria: string;
+  cadastro_config_id: IConfigData;
 }
