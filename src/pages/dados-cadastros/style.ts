6fe5ce7bef2dbe8bfd21c8d04529d8d2f36d9af4
TauDuque <hectauduque@gmail.com>
2022-02-23 16:17:25 -0300

    subj : styles dados-cadastros

---
+++ src/pages/dados-cadastros/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const CollumHide = styled.tr`
+  .first {
+    width: 0px;
+    min-width: 0;
+    max-width: 0;
+    padding: 0 0;
+    margin: 0 0;
+  }
+  .hideSeek {
+    display: none;
+  }
+
+  > th {
+    min-width: 25px !important;
+    padding: 0 5px !important;
+  }
+`;
