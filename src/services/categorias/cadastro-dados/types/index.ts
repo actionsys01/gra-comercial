f6bb7d76ab33ba4f807d2567b3370454ba1b1dd2
TauDuque <hectauduque@gmail.com>
2022-02-28 13:15:29 -0300

    subj : new types for cadastro-dados

--- src/services/categorias/cadastro-dados/types/index.ts
+++ src/services/categorias/cadastro-dados/types/index.ts
@@ -1,5 +1,39 @@
 export interface IDados {
-  id: number;
+  // [x: string]: any;
+  // id: number;
+  chave_1?: number;
+  chave_2?: string;
+  chave_3?: string;
+  chave_4?: string;
+  chave_5?: string;
+  chave_6?: string;
+  chave_7?: string;
+  chave_8?: Date;
+  cod_categoria: string;
+  desc_aplicacao: string;
+  valor_date_1?: Date;
+  valor_date_2?: Date;
+  valor_date_3?: Date;
+  valor_number_1?: number;
+  valor_number_2?: number;
+  valor_number_3?: number | any;
+  valor_number_4?: number | any;
+  valor_number_5?: number | any;
+  valor_string_1?: string;
+  valor_string_2?: string;
+  valor_string_3?: string;
+  valor_string_4?: string;
+  valor_string_5?: string;
+  valor_string_6?: string;
+  valor_string_7?: string;
+  valor_string_8?: string;
+  valor_string_9?: string;
+  valor_string_10?: string;
+}
+export interface ICreateDados {
+  // [x: string]: any;
+  id_empresa: number;
+  aplicacao: number;
   chave_1?: number;
   chave_2?: string;
   chave_3?: string;
