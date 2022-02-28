64c99c43aa8f4d9a3f24d830bb720aaef4af1599
TauDuque <hectauduque@gmail.com>
2022-02-28 15:36:19 -0300

    subj : query log commented out

--- src/pages/cadastrar-aplicativo/index.tsx
+++ src/pages/cadastrar-aplicativo/index.tsx
@@ -19,6 +19,8 @@ export default function CadastrarAplicativo() {
     desc_aplicacao: '',
   });
 
+  // console.log('router.query', router.query)
+
   const [register, setRegister] = useState({});
 
   function inputHandler(evt: any) {
