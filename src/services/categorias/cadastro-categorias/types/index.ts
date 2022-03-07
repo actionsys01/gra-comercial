c90373f81ac25d223e6df1a1c34598ece5d21a6e
TauDuque <hectauduque@gmail.com>
2022-03-07 09:55:44 -0300

    subj : new types for categories

--- src/services/categorias/cadastro-categorias/types/index.ts
+++ src/services/categorias/cadastro-categorias/types/index.ts
@@ -1,11 +1,13 @@
 import { IConfigData } from '@services/categorias/cadastro-config/types';
 
 export interface ICategories {
+  id: number;
   cod_categoria: string;
   desc_categoria: string;
 }
 
 export interface ICategoriesApps {
+  id: number;
   cod_categoria: string;
   desc_categoria: string;
   cadastro_config_id: IConfigData;
