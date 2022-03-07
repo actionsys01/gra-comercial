e762f3a962222c2981129394c2e55d7e62b00c1e
TauDuque <hectauduque@gmail.com>
2022-03-07 12:53:39 -0300

    subj : eslintfix

--- src/services/empresas/update.ts
+++ src/services/empresas/update.ts
@@ -1,16 +1,16 @@
-import api from "@services/api";
+import api from '@services/api';
 
 interface ICompanyUpdate {
-    empresa_id: number;
-    razao_social: string;
-    nome_fantasia: string;
-    cnpj: string;
-    email: string;
-    status: number;
-    plano_id: number;
-  }
+  empresa_id: number;
+  razao_social: string;
+  nome_fantasia: string;
+  cnpj: string;
+  email: string;
+  status: number;
+  plano_id: number;
+}
 
-  export default async function update(data: ICompanyUpdate) {
-      const response = await api.put("/empresas", data);
-      return response 
-  }
+export default async function update(data: ICompanyUpdate) {
+  const response = await api.put('/empresas', data);
+  return response;
+}
