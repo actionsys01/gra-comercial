ba0a6d09fbd39fca7e02a46f6541272c5ad71f7d
TauDuque <hectauduque@gmail.com>
2022-03-07 09:56:36 -0300

    subj imports new names

--- src/services/categorias/index.ts
+++ src/services/categorias/index.ts
@@ -2,7 +2,7 @@ import getCategoriesPagination from './cadastro-categorias/GetCategoriesPages';
 import CreateCategory from './cadastro-categorias/CreateCategory';
 import UpdateCategory from './cadastro-categorias/UpdateCategory';
 import DeleteCategory from './cadastro-categorias/DeleteCategory';
-import GetCategoryByService from './cadastro-categorias/GetCategoryByService';
+import GetCategoryById from './cadastro-categorias/GetCategoryById';
 import CreateConfig from './cadastro-config/CreateConfig';
 import ConfigPagination from './cadastro-config/ConfigPagination';
 import UpdateConfig from './cadastro-config/UpdateConfig';
@@ -16,7 +16,7 @@ export {
   getCategoriesPagination,
   CreateCategory,
   DeleteCategory,
-  GetCategoryByService,
+  GetCategoryById,
   UpdateCategory,
   // config
   CreateConfig,
